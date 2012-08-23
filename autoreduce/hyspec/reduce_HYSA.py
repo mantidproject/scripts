8dd612dfd293bccfc4f191f3c473170e105cc4ad
Stuart Campbell <campbellsi@ornl.gov>
2012-08-23 16:58:04 -0400

    Works with STS generated NeXus file.
    Added logic to get Ei from file and use it to calculate T0.
    Automatically determine energy bins
    Work out run number correctly from filename with .nxs.h5 ext.

--- autoreduce/hyspec/reduce_HYSA.py
+++ autoreduce/hyspec/reduce_HYSA.py
@@ -8,36 +8,42 @@ mantid_bin = sys.path.append(os.path.join(mantid_root, "bin"))
 
 from mantid.simpleapi import *
 
-#nexus_file=sys.argv[1]
-#output_directory=sys.argv[2]
+nexus_file=sys.argv[1]
+output_directory=sys.argv[2]
 
-nexus_file="/SNS/HYSA/IPTS-8072/nexus/HYSA_67.nxs.h5"
-output_directory="/SNS/HYSA/shared/autoreduce/testoutput/"
+## For testing
+#nexus_file="/SNS/HYSA/IPTS-8072/nexus/HYSA_67.nxs.h5"
+#output_directory="/SNS/HYSA/shared/autoreduce/testoutput/"
 
 filename = os.path.split(nexus_file)[-1]
-run_number = filename.split('_')[1]
+#run_number = filename.split('_')[1]
+run_number = os.path.splitext(os.path.splitext(filename.split('_')[1])[0])[0]
 
 autows = "__auto_ws"
 backgroundws = "__background_ws"
 tibws = "__tib_ws"
 
-# TODO: Get this value from the NeXus File.
-Tzero = -26.840661475400001
-
 processed_filename = os.path.join(output_directory, "HYSA_" + run_number + "_spe.nxs")
 
+# Load the data
 LoadEventNexus(Filename=nexus_file, OutputWorkspace=autows)
 
-Ei=mtd[autows].getRun()['EnergyRequest'].value
-energy_bins = "-20,0.5,20"
+# Get Ei from file
+Ei=mtd[autows].getRun()['EnergyRequest'].value[0]
+Tzero = 25.0 + 85.0/(1+(Ei/27.0)**4)
 
+# Work out some energy bins
+emin = -(2.0*Ei)
+emax = Ei-1.0
+estep = (3.0*Ei)/250
+energy_bins = "%f,%f,%f" % (emin,estep,emax)
 
 ChangeBinOffset(InputWorkspace=autows,OutputWorkspace=autows,Offset=Tzero)
 Rebin(InputWorkspace=autows,OutputWorkspace=backgroundws,Params='6000,2000,8000')
 ConvertUnits(InputWorkspace=autows,OutputWorkspace=autows,Target='DeltaE',EMode='Direct',EFixed=Ei)
 Rebin(InputWorkspace=autows,OutputWorkspace=autows,Params=energy_bins,PreserveEvents='0')
 ConvertToDistribution(Workspace=autows)
-FlatBackground(InputWorkspace=background_ws,OutputWorkspace=tibws,StartX='6000',EndX='8000',Mode='Mean',OutputMode='Return Background')
+FlatBackground(InputWorkspace=backgroundws,OutputWorkspace=tibws,StartX='6000',EndX='8000',Mode='Mean',OutputMode='Return Background')
 ConvertToDistribution(Workspace=tibws)
 Minus(LHSWorkspace=autows,RHSWorkspace=tibws,OutputWorkspace=autows)
 ConvertFromDistribution(Workspace=autows)
