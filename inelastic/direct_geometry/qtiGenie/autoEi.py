efb67fb89033be758e3fc2a9d0f04224de0a0dd8
Jon-Taylor <jon.taylor@stfc.ac.uk>
2013-03-11 21:51:51 +0000

    Added first go at autogeneration of ei and rebin
    autoEi gets a rough value of ei from monitor spectrum, using peakdet.py
    a simple peak find routine. This seems more robust than FindPeaks
    algorithum which fails to find real peaks in a reproducable manner.
    mari_auto_example.py is an example of useage for a couple of runs.
    The ei gues is hard coded to look at spectrum 2 which in any right
    minded instrument read het mari and maps is the monitor after the
    fermi, this is not the case for merlin and let where the monitors are
    located at the end of the mapping file.

---
+++ inelastic/direct_geometry/qtiGenie/autoEi.py
@@ -0,0 +1,55 @@
+# takes wkspin as string of run number 
+# returns ei, as value and rebin parameners as string
+import numpy as np
+from peakdet import *
+from mantid.simpleapi import *
+
+
+def autoEi(WkspIn):
+	monspec='2'
+	#	
+	#WkspIn='18315'
+
+	Load(Filename=WkspIn,OutputWorkspace='tmp_Monitors',Cache=r'Never',LoadLogFiles='0',SpectrumMin=monspec,SpectrumMax=monspec)
+
+	ConvertToDistribution(Workspace='tmp_Monitors')
+	ConvertUnits(InputWorkspace='tmp_Monitors',OutputWorkspace='tmp_Monitors',Target='Energy')
+	Rebin(InputWorkspace='tmp_Monitors',OutputWorkspace='tmp_Monitors',Params='2,1,2000',PreserveEvents='0')
+	#FindPeaks(InputWorkspace='tmp_Monitors',PeakFunction='Voigt',BackgroundType='Flat',HighBackground='0',PeaksList='out')
+	#ConvertTableToMatrixWorkspace(InputWorkspace='out',OutputWorkspace='out',ColumnX='centre',ColumnY='height',ColumnE='height')
+
+	dat=mtd['tmp_Monitors']
+	DeleteWorkspace('tmp_Monitors')
+
+	x=dat.extractX()		
+	y=dat.extractY()
+
+	y=y[0]
+	x=x[0]
+
+	xx=(x[1:len(x)]+x[0:len(x)-1])/2
+
+	indat=np.zeros((len(y),2))
+
+	indat[:,0]=xx
+	indat[:,1]=y
+
+
+	[maximal,miniaml]=peakdet(y, y[0]/2 ,xx)
+	#print maximal.shape
+
+	#dat=mtd['out']
+
+
+	max1=maximal[:,1].argmax()
+
+	ei=maximal[max1,0]
+	print ei
+
+	rebin=str(-ei*.5)+','+str(ei*(2.5e-3))+','+str(ei*.95)
+	#print rebin
+	#print type(rebin)
+	return ei,rebin
+	
+
+#ei,rebin=autoEi('18249')
---
+++ inelastic/user/mari_auto_example.py
@@ -0,0 +1,32 @@
+from qtiGenie import *
+from PySlice2 import *
+from autoEi import *
+inst='mar'
+iliad_setup(inst)
+ext='.raw'
+mapfile='mari_res2012'
+#det_cal_file must be specified if the reduction sends out put to a workpsace
+#cal_file='MAR17578.raw'
+cal_file='MAR18040.raw'
+#load vanadium file
+
+run='18249'
+WB='18040'
+ei,rebin_params=autoEi(str(run))
+w1=iliad(WB,run,ei,rebin_params,mapfile,det_cal_file=cal_file,norm_method='current')
+
+run='18313'
+WB='18040'
+ei,rebin_params=autoEi(str(run))
+w1=iliad(WB,run,ei,rebin_params,mapfile,det_cal_file=cal_file,norm_method='current',save_format='')
+
+run='18322'
+WB='18040'
+ei,rebin_params=autoEi(str(run))
+w1=iliad(WB,run,ei,rebin_params,mapfile,det_cal_file=cal_file,norm_method='current',save_format='')
+
+
+run='18069'
+WB='18040'
+ei,rebin_params=autoEi(str(run))
+w1=iliad(WB,run,ei,rebin_params,mapfile,det_cal_file=cal_file,norm_method='current',save_format='')
