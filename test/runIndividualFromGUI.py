151e3d1d022f32df93618eadcdbf08f2d4854cad
Alex Buts <Alex.Buts@stfc.ac.uk>
2014-02-05 16:27:28 +0000

    Modified version of runIndividualFromGUI allowing either specify list of tests from test class or run all test cases from this class.

--- test/runIndividualFromGUI.py
+++ test/runIndividualFromGUI.py
@@ -4,87 +4,93 @@ within Mantid environent
 It can be useful for debugging because the errors do not alway 'get out' of
 the sub-process used for running the tests in the regular way
 '''
-#import mantidsimple
-#print mantidsimple.__file__
 from mantid.simpleapi import *
+from mantid import config
 import sys
 import os
 import inspect
 
-
 this_dir = sys.path[0]
 print "Script resides in : ",this_dir
+#python directories
 os.chdir(r'd:\Data\MantidSystemTests\SystemTests')
 stressmodule_dir = r'd:\Data\MantidSystemTests\StressTestFramework'
 tests_dir = r'd:/Data/MantidSystemTests/SystemTests/AnalysisTests'
 
+
+sys.path.insert(0,tests_dir)
+sys.path.append(stressmodule_dir)
+
+
+#data_dir1='C:/Backup/Backup_folder1/work/code/Mantid/git/mantid/Test/systemtests/Data/SANS2D'
+#data_dir2=''C:/Backup/Backup_folder1/work/code/Mantid/git/mantid/Test/systemtests/Data/LOQ'
+
+maps_dir = 'd:/Data/MantidSystemTests/Data'
+#data_dir ='d:/Data/isis/Let/June2013'
+data_dir ='d:/Data/Isis/Let/2013_12'
+#data_dir = 'd:/ttData'
+ref_data_dir = 'd:/Data/MantidSystemTests/SystemTests/AnalysisTests/ReferenceResults' 
+config.setDataSearchDirs('{0};{1};{2}'.format(data_dir,maps_dir,ref_data_dir))
+#config.appendDataSearchDir('d:/Data/Mantid_GIT/Test/AutoTestData')
+config['defaultsave.directory'] = data_dir # folder to save resulting spe/nxspe files. Defaults are in
+
+# data search directories
+
+
 #reference_dir = 'C:/Backup/Backup_folder1/work/code/Mantid/git/mantid/Test/systemTests/SystemTests/AnalysisTests/ReferenceResults'
 #mtdpy_header_dir = 'C:/Backup/Backup_folder1/work/code/Mantid/mantid/Code/Mantid/debug'
-
-sys.path.append(r'd:/Data/Mantid_GIT/Code/win64/bin/Release')
 #sys.path.append(r'C:\Backup\Backup_folder1\work\code\Mantid\builds\all\bin\Debug')
-sys.path.append(stressmodule_dir)
-sys.path.append(r'd:\Data\MantidSystemTests\SystemTests\Data\SANS2D')
-sys.path.append(r'd:\Data\MantidSystemTests\SystemTests\Data\LOQ')
-config.setDataSearchDirs('d:/Data/MantidSystemTests/SystemTests/Data')
+
 # Find these first
-sys.path.insert(0,tests_dir)
-#Failed: 
+#modlToRun = ['ISISDirectInelastic']
+#modlToRun = ['PowderDiffProfileCalibrateTest'];
+#testToRun = ['VulcanSeqRefineProfileFromScratch'];
+#modlToRun = ['DirectInelasticDiagnostic'];
+#testToRun = ['DirectInelasticDiagnostic2'];
+#testToRun = ['DirectInelasticDiagnosticSNS','DirectInelasticDiagnostic']
+modlToRun=['ISISIndirectAbsCorTest','ISISIndirectBayesTest','ISISIndirectInelastic','ISISIndirectLoadAsciiTest'];
 
-modlToRun = ['Diffraction_Workflow_Test']
-# My: 'SXDAnalysis','WishDiffuseScattering','Diffraction_Workflow_Test',
+#modlToRun = ['Diffraction_Workflow_Test'];
+#testToRun = ['Diffraction_Workflow_Test']
+#testToRun = ['MARIReductionSum']
+#testToRun = ['MERLINReduction']
+#testToRun = ['MARIReductionFromFile','MARIReductionSum']
+#testToRun = ['MARIReductionFromWorkspace','MARIReductionFromFile','MARIReductionSum','MAPSDgreduceReduction','LETReduction','MERLINReduction']
+#testToRun = ['MAPSDgreduceReduction']
+#testToRun = ['MARIReductionFromWorkspace','MARIReductionFromFile','MARIReductionSum'];
+testToRun = []
+#,
 
 
-# not migrated 'EQSANSLive' EQSANSEff EQSANSIQOutput EQSANSTrans
-
-print "-------------------------------------------------------------------------------"
 
 
-for i in xrange(0,len(modlToRun)):
-    print "-------------------------------------------------------------------------------"        
-    print "---> Testing modue "+modlToRun[i]
-    print "-------------------------------------------------------------------------------"        
-    
-    module = __import__(modlToRun[i])
+for mod_name in modlToRun:
+
+    module = __import__(mod_name )
     reload(module)
-    
-    testsToRun = []
-    
-    for name, obj in inspect.getmembers(module):
-        if inspect.isclass(obj) and obj.__module__ == modlToRun[i] and not inspect.isabstract(obj) :
-           #print "Testing class: "+modlToRun[i]+" Name: "+name         
-            testsToRun.append(name)
+    clear_test_list = False;
+    if len(testToRun) == 0:
+        clear_test_list = True;
+        for name,class_inst in inspect.getmembers(module):
+            if inspect.isclass(class_inst):
+                if name.endswith('Test'):
+                    testToRun.append(name);
+    #reload(sys.modules['isis_reduction_steps'])
 
-            
-    if len(testsToRun)==0:
-        testsToRun.append(modlToRun[i]);
-        #print "--->  Testing class:  ",testsToRun[0]
-
-
-    for j in xrange(0,len(testsToRun)):
-        print "--->  Testing class:  ",testsToRun[j]    
-        with open("CompletedTests.txt", "a") as myfile:
-                myfile.write("Started: "+modlToRun[i]+"\t\t Test: "+testsToRun[j]+"\t finished:\t ")
-        
-        
-        className = testsToRun[j]
+    for className in testToRun:
         try:
             testClass = getattr(module, className)()
             testClass.execute()
         except:
-            with open("CompletedTests.txt", "a") as myfile:
-                myfile.write("Test: -----------------------------\n")
-            #continue
-            os.chdir(this_dir)
-            raise
-	    
-        outcome = testClass.doValidation()    
-        print 'Test result: ' + str(outcome)
-        
-        with open("CompletedTests.txt", "a") as myfile:
-                myfile.write("Test: "+testsToRun[j]+"\n")
+            #raise
+            print 'Test {0} thrown exception'.format(className);
+        #os.chdir(this_dir)
+        #raise
 
-        
-    print "-------------------------------------------------------------------------------"        
-    
+        outcome = testClass.doValidation()
+        print 'Test result: ' + str(outcome)
+
+    if clear_test_list:
+        testToRun=[];
+
 os.chdir(this_dir)
