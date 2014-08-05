ac9503cbf3608887f7d4da18267059c364637380
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2014-08-05 14:32:22 +0100

    Add calculate resolution function to ncs.py

--- development/inelastic/ncs/ncs.py
+++ development/inelastic/ncs/ncs.py
@@ -875,3 +875,38 @@ def gamma_correct(input_data,fit_options, params_ws,index=None):
                                                     WorkspaceIndexList=fit_options.workspace_index)
     return background, corrected
 
+
+#----------------------------------------------------------------------------------------
+def calculate_resolution(input_data, mass, index=0):
+    """
+        Run the VesuvioResolution function to produce a workspace
+        with the value of the Vesuvio resolution. 
+
+        @param input_data The original TOF data
+        @param mass The mass defining the recoil peak in AMU
+    """
+    from mantid.simpleapi import AlgorithmManager, mtd
+    
+    name_stem = str(input_data)
+    output_name = name_stem + "_resolution"
+    function = "name=VesuvioResolution, Mass=%f" % mass
+    
+    # execute the resolution function using fit.
+    # functions can't currently be executed as stand alone objects,
+    # so for now we will run fit with zero iterations to achieve the same result.
+    fit = mantid.api.AlgorithmManager.createUnmanaged('Fit')        
+    fit.initialize()
+    fit.setChild(True)
+    fit.setAlwaysStoreInADS(True)
+    fit.setLogging(False)
+    mantid.simpleapi._set_properties(fit, function, input_data, MaxIterations=0, 
+                                     CreateOutput=True, Output=name_stem)
+    fit.execute()
+
+    ExtractSingleSpectrum(name_stem + "_Workspace", WorkspaceIndex=1, OutputWorkspace=output_name)
+
+    DeleteWorkspace(name_stem + "_Workspace")
+    DeleteWorkspace(name_stem + "_Parameters")
+    DeleteWorkspace(name_stem + "_NormalisedCovarianceMatrix")
+
+    return mtd[output_name]
