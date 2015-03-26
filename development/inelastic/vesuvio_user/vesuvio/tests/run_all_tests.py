48ef693a54cdee2870cd082d79830358e7d79764
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-03-26 09:31:44 +0000

    Discover python tests automatic

--- development/inelastic/vesuvio_user/vesuvio/tests/run_all_tests.py
+++ development/inelastic/vesuvio_user/vesuvio/tests/run_all_tests.py
@@ -1,12 +1,21 @@
 """Run all unit tests
 """
+import os
 import unittest
 
-TEST_MODULES = ["ProfileTest", "FittingOptionsTest", "BackgroundTest", "ProcessingTest"]
+# unittest discovery was only introduced in Python 2.7 so we'll do it ourselves
+module_dir = os.path.dirname(__file__)
+test_files = os.listdir(module_dir)
+test_files.remove("__init__.py")
+test_files.remove("run_all_tests.py")
+test_modules = []
+for filename in test_files:
+    if filename.endswith(".py"):
+        test_modules.append(filename.rstrip(".py"))
 
 test_loader = unittest.defaultTestLoader
 suite = unittest.TestSuite()
-for name in TEST_MODULES:
+for name in test_modules:
     suite.addTests(test_loader.loadTestsFromName("vesuvio.tests.{0}".format(name)))
 
 unittest.TextTestRunner(verbosity=2).run(suite)
