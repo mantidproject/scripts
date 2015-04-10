8102cf54580af8ec21c585ff2972d11405d2f528
Dan Nixon <dan@dan-nixon.com>
2015-04-10 09:47:10 +0100

    Support background, use MJPEG for video encoding

--- development/inelastic/colour_plot_timelapse.py
+++ development/inelastic/colour_plot_timelapse.py
@@ -1,20 +1,52 @@
 import os
 import subprocess
 
-#TODO: Handle normalisation to a given workspace
 #TODO: Handle difference plots
 
-def generate_video(group_workspace,
+def subtract_background(workspace_group,
+                        workspace=None):
+    """
+    Subtracts a background from the scan.
+
+    Workspace parametr can wither be an integer denoting an index in the
+    workspace group or a string denoting any workspace loaded in Mantid.
+
+    @param workspace_group Name of the workspace group containing the scan
+    @param workspace String or integer denoting workspace to subtract
+    @return Name of corrected workspace group
+    """
+    background_ws = None
+
+    if workspace is not None:
+        if isinstance(workspace, int):
+            background_ws = mtd[workspace_group][workspace]
+        elif isinstance(workspace, str):
+            background_ws = mtd[workspace]
+
+    if background_ws is not None:
+        out_ws = workspace_group + '_bg_sub'
+        Minus(LHSWorkspace=workspace_group,
+              RHSWorkspace=background_ws,
+              OutputWorkspace=out_ws)
+        return out_ws
+
+    return workspace_group
+
+
+def generate_video(workspace_group,
                    directory=config['defaultsave.directry'],
                    log_names=[],
                    colour_scale=None,
                    frame_rate=10,
                    image_filename_format=r'_%d.png',
-                   encoder='avconv'):
+                   encoder=None):
     """
     Generates a timelapse video fram w workspace group.
 
-    @param group_workspace Name of the workspace group
+    If encoder parameter is not set then only a series of images
+    will be created.
+
+    @param workspace_group Name of the workspace group
     @param directory Directory in which to save frames and video
     @param log_names Sample log names to add to plot as annotation
     @param colour_scale Range for colour axis scale (None for auto)
@@ -23,7 +55,7 @@ def generate_video(group_workspace,
     @param encoder Encoder utility (avconv or ffmpeg)
     """
 
-    for i, ws in enumerate(mtd[group_workspace]):
+    for i, ws in enumerate(mtd[workspace_group]):
         # Create the plot
         plot = plot2D(ws)
         layer = plot.layer(1)
@@ -60,22 +92,33 @@ def generate_video(group_workspace,
         # Close
         plot.close()
 
-    frame_filename = os.path.join(directory, image_filename_format)
-    video_filename = os.path.join(directory, group_workspace + '.mp4')
+    if encoder is not None:
+        frame_filename = os.path.join(directory, image_filename_format)
+        video_filename = os.path.join(directory, workspace_group + '.mkv')
 
-    # Convert frames to timelapse video
-    subprocess.call([encoder,
-                     '-r', str(frame_rate),
-                     '-i', frame_filename,
-                     '-b:v', '1000k',
-                     video_filename])
+        # Convert frames to timelapse video
+        subprocess.call([encoder,
+                         '-r', str(frame_rate),
+                         '-i', frame_filename,
+                         '-c:v', 'mjpeg',
+                         '-q:v', '1',
+                         video_filename])
 
 
 # Directory to save images and final video in (best to create a new directory for this)
 output_directory = os.path.join(config['defaultsave.directory'], 'tl')
 
-# Replace 'osiris_scan' with the name of a workspace group containing workspaces to plot
-generate_video('osiris_scan',
+# Name of workspace group containing scan
+scan_ws = 'osiris_scan'
+
+# First subtract a background run
+scan_ws = subtract_background(scan_ws,
+                              workspace=0)
+
+# Create the images and video
+generate_video(scan_ws,
                directory=output_directory,
                log_names=['run_title', 'Stick'],
-               frame_rate=5)
+               colour_scale=[-8.0, 8.0],
+               frame_rate=5,
+               encoder='avconv')
