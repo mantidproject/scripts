d2f3648792fa86569e94dbb9bcd11e833918c2cd
Dan Nixon <dan@dan-nixon.com>
2015-05-08 11:00:30 +0100

    Improve documentation and script structure

--- development/inelastic/colour_plot_timelapse.py
+++ development/inelastic/colour_plot_timelapse.py
@@ -1,6 +1,20 @@
 import os
 import subprocess
 
+"""
+Script to generate a series of images and timepase video for a series of several
+reduced workspaces (e.g. a temperature scan).
+
+In order to output a video either ffmpeg or avconv must be installed and
+available on the PATH.
+On Ubuntu this can be done using "sudo apt-get install libav-tools" and setting
+the encoder option in the script to 'avconv'.
+"""
+
+
+########################
+#  WORKFLOW FUNCTIONS  #
+########################
 
 def subtract_background(workspace_group,
                         workspace=None):
@@ -67,10 +81,12 @@ def generate_difference(workspace_group):
 def generate_video(workspace_group,
                    directory=config['defaultsave.directry'],
                    log_names=[],
+                   title_log_name='run_title',
                    colour_scale=None,
                    frame_rate=10,
-                   image_filename_format=r'_%.5d.png',
-                   encoder='avconv'):
+                   image_filename_format=r'_%d.png',
+                   encoder='avconv',
+                   **kwargs):
     """
     Generates a timelapse video fram w workspace group.
 
@@ -95,10 +111,11 @@ def generate_video(workspace_group,
         if colour_scale is not None:
             layer.setAxisScale(1, *colour_scale)
 
+        run = ws.getRun()
+
         if len(log_names) > 0:
             # Generate the legend text
             legend_text = ''
-            run = ws.getRun()
             for log in log_names:
                 if log in run:
                     entry  = run[log]
@@ -114,6 +131,10 @@ def generate_video(workspace_group,
             # Add the new legend
             layer.newLegend(legend_text)
 
+        if title_log_name in run:
+            plot_title = run[title_log_name].value
+            layer.setTitle(plot_title)
+
         # Get the image filename
         image_filename = os.path.join(directory, image_filename_format % i)
 
@@ -123,7 +144,7 @@ def generate_video(workspace_group,
         # Close
         plot.close()
 
-    if encoder is not None:
+    if encoder is not None and encoder != '':
         frame_filename = os.path.join(directory, image_filename_format)
         video_filename = os.path.join(directory, workspace_group + '.mp4')
 
@@ -135,23 +156,73 @@ def generate_video(workspace_group,
                          '-q:v', '1',
                          video_filename])
 
+        print 'Video file saved to: %s' % (video_filename)
 
-#################
-# SETUP OPTIONS #
-#################
+
+def process_scan(options):
+    """
+    Contains the workflow for the data processing and video generation.
+
+    @param options Dictionary containing options
+    """
+
+    workspace = options['workspace']
+
+    if OPTIONS['replace_bad_values']:
+        # Replace any infinate or NaN values
+        out_ws_name = workspace + '_clean'
+        ReplaceSpecialValues(InputWorkspace=workspace,
+                             OutputWorkspace=out_ws_name,
+                             NaNValue=0.0,
+                             InfinityValue=0.0)
+        workspace = out_ws_name
+
+    if OPTIONS['background'] is not None:
+        # First subtract a background run
+        workspace = subtract_background(workspace,
+                                        workspace=OPTIONS['background'])
+
+    if OPTIONS['mode'] == 'difference':
+        # Generate difference workspace
+        workspace = generate_difference(workspace)
+
+    # Create the images and video
+    generate_video(workspace, **OPTIONS)
+
+
+#############
+#  OPTIONS  #
+#############
 
 OPTIONS = dict()
 
 # Name of workspace group containing scan
 # Workspaces will appear in the output in the sam eorder as they appear in the WorkspaceGroup
-SCAN_WS = 'osiris_scan'
+OPTIONS['workspace'] = 'MultiFiles'
+
+# Toggle replacement of special values (infinity and NaN) in the input data
+OPTIONS['replace_bad_values'] = True
+
+# Remove a background workspace
+# Can be either an integer denoting an index in the input workspace group
+# or a string denoting the name of a workspace
+OPTIONS['background'] = 0
+
+# Mode to plot in, options are:
+# normal: plot each of the original workspaces independently
+# difference: plots the difference between each pair of workspaces
+OPTIONS['mode'] = 'difference'
 
 # Directory to save images and final video in (best to create a new directory for this)
 OPTIONS['directory'] = os.path.join(config['defaultsave.directory'], 'tl')
 
 # List of names of log values to add to each plot image
 # (time series logs will take the average value)
-OPTIONS['log_names'] = ['run_title', 'Stick']
+OPTIONS['log_names'] = ['Stick']
+
+# Name of log entry to use as the title for each plot generated
+# If not specified the run title (log name run_title) will be used
+#OPTIONS['title_log'] = 'run_title'
 
 # Maximum and minimum values for the colour scale
 # Set to None to have this automatically adjust on a per image basis (not recommended as
@@ -164,25 +235,6 @@ OPTIONS['frame_rate'] = 5
 # Encoder utility to use to create the video
 # Options are: ffmpeg, avconv or None
 # None will create the series of images but no video
-OPTIONS['encoder'] = None
+OPTIONS['encoder'] = 'avconv'
 
-
-######################
-# PROCESS WORKSPACES #
-######################
-
-# Replace any infinate or NaN values
-ReplaceSpecialValues(InputWorkspace=SCAN_WS,
-                     OutputWorkspace=SCAN_WS,
-                     NaNValue=0.0,
-                     InfinityValue=0.0)
-
-# First subtract a background run
-SCAN_WS = subtract_background(SCAN_WS,
-                              workspace=0)
-
-# Generate difference workspace
-SCAN_WS = generate_difference(SCAN_WS)
-
-# Create the images and video
-generate_video(SCAN_WS, **OPTIONS)
+process_scan(OPTIONS)
